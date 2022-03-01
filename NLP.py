cb568038d137911075cea026f3c3abf81773631e
omarcastano <56597125+omarcastano@users.noreply.github.com>
2022-03-01 12:56:18 -0500

    Create NLP.py

---
+++ NLP.py
@@ -0,0 +1,78 @@
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+from sklearn.model_selection import train_test_split
+import unicodedata
+import re
+import numpy as np
+import os
+import io
+import time
+def get_dataset():
+
+    path_to_zip = tf.keras.utils.get_file(
+        'spa-eng.zip', origin='http://storage.googleapis.com/download.tensorflow.org/data/spa-eng.zip',
+        extract=True)
+
+    path_to_file = os.path.dirname(path_to_zip)+"/spa-eng/spa.txt"
+
+    return path_to_file
+
+# Converts the unicode file to ascii
+def unicode_to_ascii(s):
+    return ''.join(c for c in unicodedata.normalize('NFD', s)
+      if unicodedata.category(c) != 'Mn')
+
+
+def preprocess_sentence(w):
+    w = unicode_to_ascii(w.lower().strip())
+    # creating a space between a word and the punctuation following it
+    # eg: "he is a boy." => "he is a boy ."
+    # Reference:- https://stackoverflow.com/questions/3645931/python-padding-punctuation-with-white-spaces-keeping-punctuation
+
+    w = re.sub(r"([?.!,¿])", r" \1 ", w)
+    w = re.sub(r'[" "]+', " ", w)
+    
+    # replacing everything with space except (a-z, A-Z, ".", "?", "!", ",")
+    w = re.sub(r"[^a-zA-Z?.!,¿]+", " ", w)
+    w = w.strip()
+    
+    # adding a start and an end token to the sentence
+    # so that the model know when to start and stop predicting.
+    w = '<start> ' + w + ' <end>'
+    return w
+
+# 1. Remove the accents
+# 2. Clean the sentences
+# 3. Return word pairs in the format: [ENGLISH, SPANISH]
+def create_dataset(path, num_examples):
+    lines = io.open(path, encoding='UTF-8').read().strip().split('\n')
+    word_pairs = [[preprocess_sentence(w) for w in l.split('\t')]  for l in lines[:num_examples]]
+    return zip(*word_pairs)
+
+def max_length(tensor):
+    return max(len(t) for t in tensor)
+    
+
+def evaluate1(sentence,model):
+    
+    attention_plot = np.zeros((max_length_targ, max_length_inp))
+    sentence = preprocess_sentence(sentence)
+    
+    inputs = [inp_lang.word_index[i] for i in sentence.split(' ')]
+    inputs = tf.keras.preprocessing.sequence.pad_sequences([inputs],
+                                                           maxlen=max_length_inp,
+                                                           padding='post')
+    inputs = tf.convert_to_tensor(inputs)
+    result = ''
+    out = model.predict(inputs)
+    words = out.argmax(axis=-1)
+    for t in words[0]:
+        if t!=0:
+            result += targ_lang.index_word[t] + ' '
+    return result
+
+def convert(lang, tensor):
+    for t in tensor:
+        if t!=0:
+            print ("%d ----> %s" % (t, lang.index_word[t]))
